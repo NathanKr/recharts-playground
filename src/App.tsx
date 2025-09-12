0fd85cf4359a46c582fc910012c3ac4e49c3f9ac
nathankr <natankrasney@gmail.com>
2025-09-12 08:56:23 +0300

    two graphs on the same chart and more

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import {
   CartesianGrid,
+  Legend,
   Line,
   LineChart,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -13,7 +15,6 @@ const x = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1];
 const y1 = x.map((value) => value);
 const y2 = x.map((value) => value * 2);
 const data1 = x.map((value, index) => ({ x: value, y: y1[index] }));
-const data2 = x.map((value, index) => ({ x: value, y: y2[index] }));
 
 const combinedData = x.map((value, index) => ({
   x: value,
@@ -46,24 +47,41 @@ function App() {
         <Tooltip />
         <Line dataKey="y" />
       </LineChart>
-      <h2>Two graphs on the same chart</h2>
-      <LineChart width={600} height={300} data={combinedData}>
-        {/* -- dashed lines grid */}
-        <CartesianGrid strokeDasharray="3 3" />
-        {/* the label is inside the chart at its bottom with offset 5 */}
-        <XAxis
-          dataKey="x"
-          label={{ value: "X Axis", position: "insideBottom", offset: -5 }}
-        />
-        {/* the label is inside the chart and the left. the text is rotated by 90 degree */}
-        <YAxis
-          label={{ value: "Y Axis", angle: -90, position: "insideLeft" }}
-        />
-        {/* show the y value on the chart when hovered */}
-        <Tooltip />
-        <Line dataKey="y1" stroke="green" />
-        <Line dataKey="y2" stroke="blue" />
-      </LineChart>
+      <h2>
+        Two graphs on the same chars ,legened ,responsive ,thick lines no dots
+      </h2>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={combinedData}>
+          {/* -- dashed lines grid */}
+          <CartesianGrid strokeDasharray="3 3" />
+          {/* the label is inside the chart at its bottom with offset 5 */}
+          <XAxis
+            dataKey="x"
+            label={{ value: "X Axis", position: "insideBottom", offset: -5 }}
+          />
+          {/* the label is inside the chart and the left. the text is rotated by 90 degree */}
+          <YAxis
+            label={{ value: "Y Axis", angle: -90, position: "insideLeft" }}
+          />
+          {/* show the y value on the chart when hovered */}
+          <Tooltip />
+          <Legend verticalAlign="top" />
+          <Line
+            dataKey="y1"
+            stroke="green"
+            strokeWidth={2}
+            name="Line 1"
+            dot={false}
+          />
+          <Line
+            dataKey="y2"
+            stroke="brown"
+            strokeWidth={2}
+            name="Line 2"
+            dot={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </>
   );
 }
